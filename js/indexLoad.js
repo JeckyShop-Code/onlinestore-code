211e6f29ee201a3909bb66ae3a9756ed542390a6
Jean Carlos <jcarlos.ordonez01@gmail.com>
2024-08-16 15:11:40 -0500

    loader

--- js/indexLoad.js
+++ js/indexLoad.js
@@ -1,5 +1,5 @@
-window.onload =  ()=> {
+document.addEventListener('DOMContentLoaded',()=> {
     const loader = document.getElementById('onload');
     loader.remove();
     document.body.classList.remove('hidden');
-};
+});
--- js/main.js
+++ js/main.js
@@ -1,45 +1,43 @@
 import { renderedCard } from "./components/generateProductCard.js";
 import { searchByCategory } from "./components/productsSearchAlgoritm.js";
 
-document.addEventListener('DOMContentLoaded', ()=> {
-    const dataUrl = '../data/products.json';
+const dataUrl = '../data/products.json';
 
-    function viewProductsCategory(category = '', slider) {
-        fetch(dataUrl)
-            .then(response => response.json())
-            .then(data => renderedCard(slider, searchByCategory(category, data, 6)))
+function viewProductsCategory(category = '', slider) {
+    fetch(dataUrl)
+        .then(response => response.json())
+        .then(data => renderedCard(slider, searchByCategory(category, data, 6)))
+}
+
+const accessoriesCategorySlider = document.getElementById("ac-slider");
+const technologyCategorySlider = document.getElementById("tc-slider");
+const beautyCategorySlider = document.getElementById("bc-slider");
+const homeCategorySlider = document.getElementById("hc-slider");
+const toolsCategorySlider = document.getElementById("tlc-slider");
+
+viewProductsCategory('Accesorios', accessoriesCategorySlider);
+viewProductsCategory('Tecnologia', technologyCategorySlider);
+viewProductsCategory('Belleza', beautyCategorySlider);
+viewProductsCategory('Hogar', homeCategorySlider);
+viewProductsCategory('Herramientas', toolsCategorySlider);
+
+const inputSearch = document.getElementById('home-search-input');
+const btnSearch = document.getElementById('btn-home-search');
+
+btnSearch.addEventListener('click', () => {
+    const inputValue = inputSearch.value;
+
+    if (inputValue == '') {
+        alert('No olvides ingresar el nombre del producto que deseas buscar!')
+        return;
     }
 
-    const accessoriesCategorySlider = document.getElementById("ac-slider");
-    const technologyCategorySlider = document.getElementById("tc-slider");
-    const beautyCategorySlider = document.getElementById("bc-slider");
-    const homeCategorySlider = document.getElementById("hc-slider");
-    const toolsCategorySlider = document.getElementById("tlc-slider");
+    sessionStorage.setItem('inputProductName', inputValue)
+    window.open('templates/catalog/catalog.html', '_self')
+});
 
-    viewProductsCategory('Accesorios', accessoriesCategorySlider);
-    viewProductsCategory('Tecnologia', technologyCategorySlider);
-    viewProductsCategory('Belleza', beautyCategorySlider);
-    viewProductsCategory('Hogar', homeCategorySlider);
-    viewProductsCategory('Herramientas', toolsCategorySlider);
-
-    const inputSearch = document.getElementById('home-search-input');
-    const btnSearch = document.getElementById('btn-home-search');
-
-    btnSearch.addEventListener('click', () => {
-        const inputValue = inputSearch.value;
-
-        if (inputValue == '') {
-            alert('No olvides ingresar el nombre del producto que deseas buscar!')
-            return;
-        }
-
-        sessionStorage.setItem('inputProductName', inputValue)
-        window.open('templates/catalog/catalog.html', '_self')
-    });
-
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            btnSearch.click();
-        }
-    });
-})
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        btnSearch.click();
+    }
+});
