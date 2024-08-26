f94f91f2a9b57239a5c305775d5e365e3acfd5ac
Jean Carlos <jcarlos.ordonez01@gmail.com>
2024-08-26 09:44:42 -0500

    redireccion

--- js/product_redirect.js
+++ js/product_redirect.js
@@ -1,7 +1,8 @@
+
 const urlParams = new URLSearchParams(window.location.search);
 const productId = urlParams.get('product');
 
 if (productId != null) {
-    redirect_url = `http://localhost:5500/templates/product/product-information.html?productId=${productId}`
+    redirect_url = `http://${window.location.hostname}/templates/product/product-information.html?productId=${productId}`
     window.open(redirect_url, '_self')
 }
